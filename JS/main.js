30caa74f3ee2585836e458b252f99453fcc361b0
sarkar baske <sarkarbaske28@gmail.com>
2026-01-08 21:06:12 +0545

    add index main and readme files

---
+++ JS/main.js
@@ -0,0 +1,62 @@
+const fname="sarkar baske"
+console.log(fname)
+console.error("This is error message")
+console.info("This is information")
+console.warn("this is warning")
+
+/** variable types
+ * string
+ * array
+ * boolean false ,true
+ * object
+ * number
+ * null, undefined
+ */
+
+//var - you can define multiple var value for the same variable THIS NEVER USED DURING PROGRAMMING
+//const value can't be change once assign
+//let can define variable that can't be assign again but value can be change
+
+let markss=20
+markss=40
+markss=50
+console.log(markss)
+
+const sname="sarkar Baske"
+// sname="rohon baske" -> This is can't allow in constant value assign
+console.log(sname)
+
+let F_name="sakrar"
+let L_name="Baske"
+let address="Mechinagar-15"
+let gpa=4.00
+console.log("My Name is",F_name,L_name)
+
+// array= list of different items
+// items are indicated using index no. start from zero for the first items
+
+let marks=[10,'sarkar','baske',20,50,{name:"sakrar baske", rollNo:10,level:"Diploma"},98343]
+console.log(marks[5].rollNo)
+
+//object key pair value is assigned in object
+
+const student={
+    sarkar_baske:{
+        fullname:"sarkar baske",
+        Roll: 20,
+        faculty:"diploma in computer engineering"
+    },
+     Rohon_baske:{
+        fullname:"rohon baske",
+        Roll: 30,
+        faculty:"diploma in civil engineering"
+    },
+     jiban_ghor:{
+        fullname:"jiban ghor",
+        Roll: 40,
+        faculty:"diploma in electronic engineering"
+    },
+}
+console.log(student.jiban_ghor)
+console.log(student.sarkar_baske)
+console.log(student.Rohon_baske)
---
+++ README.md
@@ -0,0 +1,18 @@
+* javascript programming language
+- Hight level programming language
+-Interpreted programming language
+-ECMA (ES6) standardization of js
+-Used to build interactive web pages
+-Node.js, Electron JS, React Native, Tensorflow.js
+
+
+--------
+-Js
+* BackEnd
+-Node js
+-Express
+-MangoDB
+
+* FrontEnd
+-React
+-Next
